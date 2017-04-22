4703097d9fabda6477c1d906209ce374f5049cb6
nanako <o.o.nanako.o.o@gmail.com>
2017-04-22 17:27:55 +0900

    発展まだ

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    
+    var number1: Int = 0
+    var number2: Int = 0
+    var number3: Int = 0
+    
+    var ope: Int = 0
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,69 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func three(){
+        
+        number1 = number1*10+3
+        label.text = String(number1)
+    }
+    
+   
+    @IBAction func four(){
+            
+            number1 = number1*10+4
+            label.text = String(number1)
+        }
+    
+    @IBAction func plus(){
+        
+        number2 = number1
+        number1 = 0
+        ope = 1
+        
+    }
+    @IBAction func mainasu(){
+        
+        number2 = number1
+        number1 = 0
+        ope = 2
+        
+    }
+    @IBAction func kakeru(){
+        
+        number2 = number1
+        number1 = 0
+        ope = 3
+        
+    }
+    @IBAction func waru(){
+        
+        number2 = number1
+        number1 = 0
+        ope = 4
+        
+    }
 
+    
+    @IBAction func equal(){
+        
+        if ope == 1{
+            number3 = number1 + number2}
+            
+        else if ope == 2{
+            number3 = number1 - number2}
+            
+        else if ope == 3{
+        number3 = number1*number2}
+            
+        else if ope == 4{
+            number3 = number2 / number1}
 
-}
+        
+                        label.text = String(number3)}
+
+        
+    }
+
+    
+
 
